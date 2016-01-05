a4c2c2496c6999faff431a8d544373a7da7ba945
xiaobai <dx5878210@gmail.com>
2016-01-05 15:56:52 +0800

    hook test1

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from PMtools import app
 from flask.ext.script import Manager,Server
-#hello 1
+
 manager = Manager(app)
 manager.add_command(
     "runserver",
     Server(
         host="127.0.0.1",
-        port=8000,
+        port=8181,
         use_debugger=True))
 if __name__ == '__main__':
     manager.run()
