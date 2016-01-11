102baecd50c670429a396a0c9fca87b6bdfb1c5e
xiaobai <dx5878210@gmail.com>
2016-01-11 14:25:11 +0800

    commit ignore test

--- config/default.py
+++ config/default.py
@@ -4,8 +4,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(basedir, 'data.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
-
 DEBUG = False
 SQLALCHEMY_ECHO = False
 
-#test
+# test
