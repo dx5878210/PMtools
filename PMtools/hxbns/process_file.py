7e8dcdf245b61d62b16aed0824ff450fede8a96b
xiaobai <dx5878210@gmail.com>
2016-01-26 18:54:55 +0800

    跨区test

--- PMtools/hxbns/process_file.py
+++ PMtools/hxbns/process_file.py
@@ -15,7 +15,7 @@ class RequestsMethods(object):
 
     def get_cookie(self):
         #login_url = 'http://tw-hxbns.gm.funcell123.com/index.php?r=sys/user/login'
-        cookie_request = requests.get('http://tw-hxbns.gm.funcell123.com/index.php?r=sys/user/login')
+        cookie_request = requests.get('http://46.101.85.29/index')
         cookie_request.encoding = 'utf-8'
         # cookies = dict(
         #     PHPSESSID=cookie_request.cookies['PHPSESSID'],
