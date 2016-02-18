a5a7727d059055c6fa850d485d023ab85e92e1ef
xiaobai <dx5878210@gmail.com>
2016-02-18 18:16:38 +0800

    跨区test

--- PMtools/hxbns/views.py
+++ PMtools/hxbns/views.py
@@ -54,16 +54,16 @@ def hxbnsupload():
 def postvlc():
     validateCode = request.args.get('vlc')###存在bug不能使用request
     rm = process_file.RequestsMethods()
-    rm.setcookie(session['user'])
-    print (session)
-    if rm.login(validateCode):
-        print("登录成功")
-        return render_template('hxbns/fileupload.html')
-    else:
-        print("登录失败")
-        flash('You were successfully logged in')
-        session.clear()
-        return render_template('hxbns/submitvalidate.html')
+    #rm.setcookie(session['user'])
+    # print (session)
+    # if rm.login(validateCode):
+    #     print("登录成功")
+    #     return render_template('hxbns/fileupload.html')
+    # else:
+    #     print("登录失败")
+    #     flash('You were successfully logged in')
+    #     session.clear()
+    #     return render_template('hxbns/submitvalidate.html')
     return str(validateCode)
 
 
